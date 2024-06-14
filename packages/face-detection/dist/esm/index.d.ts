4b8bf2d5b21155528b3143e5045ff7923649c12b
Arthur <arthurbragagrimaldi14@gmail.com>
2024-06-14 10:52:24 -0300

    Revert "builded face-detection"
    This reverts commit 0c93cfe4a97453b06eb498348147af8b94c6c3fe.

--- packages/face-detection/dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { FaceDetectionPlugin } from './definitions';
-declare const FaceDetection: FaceDetectionPlugin;
-export * from './definitions';
-export { FaceDetection };
--- packages/face-detection/dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const FaceDetection = registerPlugin('FaceDetection', {
-    web: () => import('./web').then(m => new m.FaceDetectionWeb()),
-});
-export * from './definitions';
-export { FaceDetection };
-//# sourceMappingURL=index.js.map
