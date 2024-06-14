ec249b09a739df8fcd1b090a77cb82fa1c0de7af
Arthur <arthurbragagrimaldi14@gmail.com>
2024-06-14 10:54:13 -0300

    removed unchanged files

--- face-detection/dist/esm/web.d.ts
+++
@@ -1,6 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-import type { FaceDetectionPlugin, ProcessImageOptions, ProcessImageResult } from './definitions';
-export declare class FaceDetectionWeb extends WebPlugin implements FaceDetectionPlugin {
-    processImage(_options: ProcessImageOptions): Promise<ProcessImageResult>;
-    private createUnavailableException;
-}
